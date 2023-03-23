a2c8bcce186ad47b0e97c96610b9869794ad454a
何知曲成溪 <1242079396@qq.com>
2023-03-23 23:16:22 +0800

    proxy

---
+++ day01/proxy-demo.js
@@ -0,0 +1,25 @@
+const data = {
+  name:'qwe',
+  age:18
+}
+
+const proxyData = new Proxy(data,{
+  get(target, key, receiver) {
+    const result = Reflect.get(target,key,receiver)
+    console.log('get',key);
+    return result
+  },
+  set(target,key,val,receiver) {
+    const result = Reflect.set(target,key,val,receiver)
+    console.log('set',key,val);
+    console.log('result',result);
+    return result //是否设置成功
+  },
+  deleteProperty(target,key) {
+    const result = Reflect.deleteProperty(target,key)
+    console.log('delete property', key);
+    console.log('result', result);//true
+    return result
+  }
+
+})
